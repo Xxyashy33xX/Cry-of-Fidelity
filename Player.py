95798b1b47d54ac5160075853f89198cc019a24f
Xxyashy33xX <153779757+Xxyashy33xX@users.noreply.github.com>
2024-04-05 01:59:55 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,13 +3,13 @@ import pygame as pg
 import math
 
 class Player:
-    def __init__(self, game):
+    def __init__(self, game): # initialise 
         self.game = game
         self.x, self.y = Player_Pos
         self.angle = Player_Angle
 
     def movement(self):
-        sin_a = math.sin(self.angle)
+        sin_a = math.sin(self.angle) # to find the direction the player is looking at
         cos_a = math.cos(self.angle)
         dx, dy = 0, 0
         speed = Player_Speed * self.game.delta_time
@@ -18,19 +18,19 @@ class Player:
 
         keys = pg.key.get_pressed()
         num_key_pressed = -1
-        if keys[pg.K_w]:
+        if keys[pg.K_w]: #the player presses "W"
             num_key_pressed += 1
             dx += speed_cos
             dy += speed_sin
-        if keys[pg.K_s]:
+        if keys[pg.K_s]: #the player presses "S"
             num_key_pressed += 1
             dx += -speed_cos
             dy += -speed_sin
-        if keys[pg.K_a]:
+        if keys[pg.K_a]: #the player presses "A"
             num_key_pressed += 1
             dx += speed_sin
             dy += -speed_cos
-        if keys[pg.K_d]:
+        if keys[pg.K_d]: #the player presses "D"
             num_key_pressed += 1
             dx += -speed_sin
             dy += speed_cos
@@ -43,7 +43,7 @@ class Player:
     def check_wall(self, x, y):
         return (x, y) not in self.game.map.world_map
 
-    def check_wall_collision(self, dx, dy):
+    def check_wall_collision(self, dx, dy): # checks if the player has gone inside the wall, and if so stops movement
 
         if self.check_wall(int(self.x + dx), int(self.y)):
             self.x += dx
@@ -51,11 +51,11 @@ class Player:
             self.y += dy
         self.angle %= math.tau
 
-    def draw(self):
+    def draw(self): # draws a white line to show where the player is looking at in the 2D map
         pg.draw.line(self.game.screen, "white", (self.x * 100, self.y * 100),
                      (self.x * 100 + width * math.cos(self.angle),
                       self.y * 100 + width * math.sin(self.angle)), 2)
-        pg.draw.circle(self.game.screen, "red", (self.x * 100, self.y * 100), 15)
+        pg.draw.circle(self.game.screen, "red", (self.x * 100, self.y * 100), 15) # The shape of the player in the 2D map
 
     def mouse_control(self):
         # Get the current mouse position
@@ -72,10 +72,10 @@ class Player:
 
 
 
-    def update(self):
+    def update(self): # outputs the movements and mouse controls to the update method
         self.movement()
         self.mouse_control()
-
+# to give "special" functionality to certain methods to make them act as getters, setters or deleters when we define properties in a class.
     @property
     def pos(self):
         return self.x, self.y
