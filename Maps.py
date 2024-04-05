dbad8012b4b97f13488e9845f3526ea0f5971427
Xxyashy33xX <153779757+Xxyashy33xX@users.noreply.github.com>
2024-04-05 01:42:03 +0100

    Update Maps.py

--- Maps.py
+++ Maps.py
@@ -28,16 +28,17 @@ mini_map = [
     [1, _, _, _, _, _, _, _, _, _, _, _, 5, 1, _, _, _, _, _, 1, _, 1, _, 1, 3, _, _, _, 4, 2, _, _],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 
-]
+] # The 2D array that is the map of the game
+  # Numeric values are a specific texture, and a "_" is empty space
 
-class Map:
+class Map: # initialises variables
     def __init__(self, game):
         self.game = game
         self.mini_map = mini_map
         self.world_map = {}
         self.get_map()
 
-    def get_map(self):
+    def get_map(self): # traverses the array and outputs the value
         for j, row in enumerate(self.mini_map):
             for i, value in enumerate(row):
                 if value:
@@ -46,6 +47,9 @@ class Map:
     def draw(self):
         [pg.draw.rect(self.game.screen, "green", (pos[0] * 100, pos[1] * 100, 100, 100), 2)
          for pos in self.world_map]
+        # outputs a square for any numeric value in that specific position
+
+
 
 
 
